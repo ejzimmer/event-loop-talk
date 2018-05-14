ab0cca40c20ed8989222b11cae12b178925c254a
ejzimmer@gmail.com <erin.zimmer@auspost.com.au>
2018-05-14 20:41:52 +1000

    next tick

--- node-test.js
+++ node-test.js
@@ -1,49 +1,25 @@
 const fs = require('fs');
 
-setTimeout(() => {
-  console.log('timeout');
+fs.readFile('index.html', () => {
   process.nextTick(() => {
-    console.log('tick')
-    process.nextTick(() => {
-      console.log('tock');
+    console.log('tick');
+    Promise.resolve().then(() => {
+      console.log('promise');
       process.nextTick(() => {
-        console.log('fick');
-        process.nextTick(() => {
-          console.log('fock');
+        console.log('promise tick');
+        Promise.resolve().then(() => {
+          console.log('promise 2');
+          process.nextTick(() => {
+            console.log('promise 2 tick');
+          })
         })
-      })
+      });
     })
-  });
-})
-setTimeout(() => {
-  console.log('timeout2');
-  process.nextTick(() => console.log('tick2'));
-})
-setTimeout(() => {
-  console.log('timeout3');
-  process.nextTick(() => console.log('tick3'));
-})
-setImmediate(() => {
-  console.log('immediate4');
-  process.nextTick(() => console.log('tick4'));
-})
-setTimeout(() => {
-  console.log('timeout5');
-  process.nextTick(() => console.log('tick5'));
-})
-setTimeout(() => {
-  console.log('immediate6');
-  process.nextTick(() => console.log('tick6'));
-})
-setTimeout(() => {
-  console.log('timeout7');
-  process.nextTick(() => console.log('tick7'));
-})
-setTimeout(() => {
-  console.log('immediate8');
-  process.nextTick(() => console.log('tick8'));
-})
-setTimeout(() => {
-  console.log('timeout9');
-  process.nextTick(() => console.log('tick9'));
-})
+    setImmediate(() => {
+      console.log('immediate');
+      setImmediate(() => console.log('immediate 2'));
+      process.nextTick(() => console.log('immediate 2 tick'));
+    })
+  })
+});
+
